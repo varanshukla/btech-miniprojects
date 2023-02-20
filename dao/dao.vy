6e41b661a93299e0b8f75c8247e6871cf4bd9eec
Kai Mast <kaimast@cs.wisc.edu>
2023-02-20 15:18:34 -0600

    Add some more skeleton code

--- dao/dao.vy
+++ dao/dao.vy
@@ -2,4 +2,22 @@ from vyper.interfaces import ERC20
 
 implements: ERC20
 
+event Transfer:
+    sender: indexed(address)
+    receiver: indexed(address)
+    value: uint256
 
+event Approval:
+    owner: indexed(address)
+    spender: indexed(address)
+    value: uint256
+
+balanceOf: public(HashMap[address, uint256])
+allowance: public(HashMap[address, HashMap[address, uint256]])
+totalSupply: public(uint256)
+
+@external
+def __init__():
+    self.totalSupply = 0
+
+#TODO add the rest here
--- dao/test_dao.py
+++ dao/test_dao.py
@@ -1,15 +1,13 @@
 #pylint: disable=missing-docstring,redefined-outer-name
 
 import pytest
-from eth_tester.exceptions import TransactionFailed
 
 @pytest.fixture
 def dao_contract(w3, get_vyper_contract):
     with open("dao.vy", encoding='utf-8') as infile:
         contract_code = infile.read()
 
-    args = [w3.eth.accounts[0], w3.eth.accounts[1]]
-    return get_vyper_contract(contract_code, *args)
+    return get_vyper_contract(contract_code)
 
 def test_nothing(dao_contract):
     pass
